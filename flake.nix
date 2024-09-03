aa06326d498acb8cf002950847ea570ff453c4b3
Berk Özkütük <berk@ozkutuk.me>
2024-09-03 15:25:37 +0200

    chore: init

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "migadu-hs";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: let
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+        myOverlay = pkgs.haskell.lib.compose.packageSourceOverrides {
+          migadu = ./.;
+        };
+        hsPkgs = pkgs.haskell.packages.ghc982.extend myOverlay;
+
+      in {
+        devShells.default = hsPkgs.shellFor {
+          packages = p: [p.migadu];
+          nativeBuildInputs = [
+            hsPkgs.cabal-install
+            hsPkgs.haskell-language-server
+            hsPkgs.fourmolu
+            hsPkgs.cabal-fmt
+          ];
+        };
+
+        packages.default = hsPkgs.migadu;
+
+        formatter = pkgs.alejandra;
+      };
+    };
+}
