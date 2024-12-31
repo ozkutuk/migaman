5119e3a923bf6d1a76d10a8de3c1ec7c06e51710
Berk Özkütük <berk@ozkutuk.me>
2024-12-31 01:29:26 +0100

    feat(migaman): implement import

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,6 @@
 module Main where
 
 import qualified MyLib
--- import qualified Migadu
 
 main :: IO ()
 main = MyLib.main
--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE NoFieldSelectors #-}
 
@@ -18,11 +19,14 @@ import Database.Beam qualified as Beam
 import Database.Beam.Sqlite qualified as Beam
 import Database.SQLite.Simple qualified as Sqlite
 
-import Control.Monad.IO.Class (liftIO)
 import Data.Functor.Identity (Identity)
 import Data.Int (Int64)
 import Data.Text (Text)
 import GHC.Generics (Generic)
+
+import Migadu ()
+import Migadu qualified
+import System.Exit (die)
 
 data IdentityTable f = Identity'
   { id :: Columnar f Int64
@@ -54,9 +58,36 @@ newtype MigamanDb f = MigamanDb
 migamanDb :: DatabaseSettings be MigamanDb
 migamanDb = Beam.defaultDbSettings
 
-main :: IO ()
-main = do
+migaduIdentities :: IO (Migadu.Identities Migadu.Read)
+migaduIdentities = do
+  mAuth <- Migadu.getAuth
+  case mAuth of
+    Nothing -> die "invalid auth"
+    Just auth ->
+      let idAct = Migadu.IdentitiesIndex "example.com" "target"
+       in Migadu.runMigadu auth idAct
+
+toIdentityTable :: Migadu.Identity Migadu.Read -> IdentityTable (Beam.QExpr Beam.Sqlite s)
+toIdentityTable identity =
+  Identity'
+    { id = Beam.default_
+    , account = Beam.val_ identity.localPart
+    , localpart = Beam.val_ identity.localPart
+    }
+
+toIdentityTableAll :: Migadu.Identities Migadu.Read -> [IdentityTable (Beam.QExpr Beam.Sqlite s)]
+toIdentityTableAll (Migadu.Identities ids) = map toIdentityTable ids
+
+importIdentities' :: Migadu.Identities Migadu.Read -> Beam.SqlInsert Beam.Sqlite IdentityTable
+importIdentities' = Beam.insert migamanDb.identity . Beam.insertData . toIdentityTableAll
+
+importIdentities :: IO ()
+importIdentities = do
+  identities <- migaduIdentities
   conn <- Sqlite.open "database.sqlite3"
   Beam.runBeamSqliteDebug putStrLn conn $ do
-    identities <- Beam.runSelectReturningList $ Beam.select (Beam.all_ migamanDb.identity)
-    liftIO $ print identities
+    Beam.runInsert $ importIdentities' identities
+
+main :: IO ()
+main = do
+  importIdentities
