e4b2a173cdc907430054a7744b11f07939e04b47
Berk Özkütük <berk@ozkutuk.me>
2025-03-22 00:33:38 +0100

    build: add static build

--- flake.nix
+++ flake.nix
@@ -25,11 +25,24 @@
         ...
       }: let
         pkgs = inputs.nixpkgs.legacyPackages.${system};
+        pkgsStatic = inputs.nixpkgs.legacyPackages.${system}.pkgsMusl;
         myOverlay = pkgs.haskell.lib.compose.packageSourceOverrides {
           migaman = ./.;
           migadu = ./migadu;
         };
         hsPkgs = pkgs.haskell.packages.ghc982.extend myOverlay;
+        fixGhc = pkg: pkg.override {
+          enableRelocatedStaticLibs = true;
+          enableShared = false;
+          enableDwarf = false;
+        };
+        hsPkgsStatic = (pkgsStatic.haskell.packages.ghc982.override (old: {
+          ghc = fixGhc old.ghc;
+          buildHaskellPackages = old.buildHaskellPackages.override (oldBHP: {
+            ghc = fixGhc oldBHP.ghc;
+          });
+        })).extend myOverlay;
+
       in {
         devShells.default = hsPkgs.shellFor {
           packages = p: [p.migaman p.migadu];
@@ -46,6 +59,27 @@
         };
 
         packages.migaman = pkgs.haskell.lib.justStaticExecutables hsPkgs.migaman;
+        packages.migaman-static = pkgs.haskell.lib.overrideCabal hsPkgsStatic.migaman (old: {
+          postInstall = (old.postInstall or "") + ''
+            for b in $out/bin/*
+            do
+              if ldd "$b"
+              then
+                echo "ldd succeeded on $b, which may mean that it is not statically linked"
+                exit 1
+              fi
+            done
+          '';
+          configureFlags = (old.configureFlags or [ ]) ++ [
+            "--ghc-option=-optl=-static"
+            "--extra-lib-dirs=${pkgsStatic.gmp6.override { withStatic = true; }}/lib"
+            "--extra-lib-dirs=${pkgsStatic.libffi.overrideAttrs (old: { dontDisableStatic = true; })}/lib"
+            "--extra-lib-dirs=${pkgsStatic.zlib.static}/lib"
+          ];
+          enableSharedExecutables = false;
+          enableSharedLibraries = false;
+        });
+
         packages.default = config.packages.migaman;
 
         formatter = pkgs.alejandra;
